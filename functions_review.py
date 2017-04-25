e4197f77c03cf94a5cf374f6f6b684718918c14a
Zack Spann <zspann@zspann-macbookpro3.roam.corp.google.com>
2017-04-25 12:30:18 -0700

    It all works

--- functions_review.py
+++ functions_review.py
@@ -1,18 +1,19 @@
 
 #Division Function: given two numbers returns quotient of the first number divided by the second number
 def division(num1, num2):
-    return "your code here"
+    return num1 / num2
 
-#Uncomment the rest of these functions and get them to pass the tests!  
+#Uncomment the rest of these functions and get them to pass the tests!
 
 # Greeting function: Concatenates two arguments and adds an exclamation point.
-# def greeting:
+def greeting(arg1, arg2):
+    return arg1 + " " + arg2 + "!"
 
 # Return a value: Returns the string "Have a nice day!"
-# def return_a_value:
-    
+def return_a_value():
+    return "Have a nice day!"
 
 #Pizza Party: Has an optional argument that defaults to "cheese" and returns "cheese" when called without an argument
 
-# def pizza_party:
-
+def pizza_party(flav = "Cheese"):
+    return flav
